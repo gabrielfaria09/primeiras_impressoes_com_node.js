79974388b98b51b5f58cd4c16ec480852d6ef3ee
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-08-15 14:36:33 -0300

    Testes finais com banco de dados local e requisição GET e  DELETE.

--- servidor_curso/database.js
+++ servidor_curso/database.js
@@ -2,8 +2,24 @@ import { randomUUID } from "node:crypto"
 export class Database {
     #videos = new Map();
 
-    list() {
-      return Array.from(this.#videos.values());
+    list(search) {
+      return Array.from(this.#videos.entries())
+      .map((videoArray) => {
+        const id = videoArray[0];
+        const data = videoArray[1];
+
+        return {
+            id,
+            ...data,
+        }
+      })
+      .filter(video => {
+        if (search) {
+            return video.title.includes(search);
+        }
+
+        return true;
+      })
     }
     
     create(video) {
--- servidor_curso/servidor.js
+++ servidor_curso/servidor.js
@@ -4,8 +4,17 @@ import { Database } from "./database.js";
 const servidor = fastify();
 const database = new Database();
 
-servidor.put("/videos/:id", () => {
-  return "Hello World";
+servidor.put("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title,
+    description,
+    duration
+  });
+
+  return reply.status(204).send();
 });
 
 servidor.post("/videos", (request, reply) => {
@@ -20,16 +29,22 @@ servidor.post("/videos", (request, reply) => {
   return reply.status(201).send();
 });
 
-servidor.get("/videos", () => {
-  const videos = database.list();
+servidor.get("/videos", (request) => {
+  const search = request.query.search;
+
+  const videos = database.list(search);
 
   console.log(videos);
 
   return videos;
 });
 
-servidor.delete("/videos/:id", () => {
-  return "Hello Node.js";
+servidor.delete("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
 });
 
 servidor.listen({
